3c6f0069005741afc6e83c9b4adf4fd89cdecee6
T <tyoungjr2005@gmail.com>
2021-10-18 17:36:17 -0500

    A CmakeCpp project template

---
+++ src/empty.cpp
@@ -0,0 +1,17 @@
+// ==========================================================================
+//
+// Software written by Boguslaw Cyganek (C) to be used with the book:
+// INTRODUCTION TO PROGRAMMING WITH C++ FOR ENGINEERS
+// Published by Wiley, 2020
+//
+// The software is supplied as is and for educational purposes
+// without any guarantees nor responsibility of its use in any application. 
+//
+// ==========================================================================
+
+
+
+#include "empty.h"
+
+
+
---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+// ==========================================================================
+//
+// Software written by Boguslaw Cyganek (C) to be used with the book:
+// INTRODUCTION TO PROGRAMMING WITH C++ FOR ENGINEERS
+// Published by Wiley, 2020
+//
+// The software is supplied as is and for educational purposes
+// without any guarantees nor responsibility of its use in any application. 
+//
+// ==========================================================================
+
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+#include "empty.h"
+
+
+int main()
+{
+	std::cout << "Good day!" << std::endl;
+	return 0;
+}
+
+
