b754b1c34d32fc819c97c9de69b3bb89d929729a
T <tyoungjr2005@gmail.com>
2021-10-19 11:45:56 -0500

    Cleaning up main function

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,11 @@
 //
 // ==========================================================================
 
-
 // Macro to determine whether to run tests or skip the runner
-// Unfortunately build times will still build the catch library which isn't ideal
-// for releases.
 #define TESTS_ENABLED 1
-
-
 #define CATCH_CONFIG_RUNNER
 #include <catch2/catch.hpp>
-#include <string>
-#include <vector>
-#include <algorithm>
 #include <iostream>
-
 #include "empty.h"
 
 
